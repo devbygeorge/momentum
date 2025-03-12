734b5f6435f0fe73aa0011decc4fd418acc322ad
devbygeorge <giopasieshvili2@gmail.com>
2025-03-12 20:21:05 +0400

    move fonts and icons in assets folder

---
+++ src/assets/fonts/index.ts
@@ -0,0 +1,22 @@
+import localFont from "next/font/local";
+
+export const firago = localFont({
+  src: [
+    {
+      path: "./FiraGO-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./FiraGO-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./FiraGO-SemiBold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+  ],
+  variable: "--font-firago",
+});
