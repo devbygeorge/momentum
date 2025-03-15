426d69eeb36138a0e872a24cd9556290e410969e
devbygeorge <giopasieshvili2@gmail.com>
2025-03-15 16:18:51 +0400

    allow redberry images hostname

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,14 @@ const nextConfig: NextConfig = {
     });
     return config;
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "momentum.redberryinternship.ge",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
