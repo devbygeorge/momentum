ee55afc5664eb45165d6e43bc45968c500e9b4a0
devbygeorge <giopasieshvili2@gmail.com>
2025-03-16 12:06:37 +0400

    use custom scrollbar in columns

--- src/features/kanban/Column.tsx
+++ src/features/kanban/Column.tsx
@@ -1,6 +1,8 @@
 import { Task } from "@/types";
 import s from "./Column.module.css";
 import TaskCard from "./TaskCard";
+import "simplebar-react/dist/simplebar.min.css";
+import SimpleBar from "simplebar-react";
 
 type ColumnProps = {
   statusId: number;
@@ -14,12 +16,13 @@ export default function Column({ statusId, statusName, tasks }: ColumnProps) {
       <h2 className={`${s.heading} ${s[`status-${statusId}`]}`}>
         {statusName}
       </h2>
-
-      <div className={s.list}>
-        {tasks?.map((task) => (
-          <TaskCard key={task.id} task={task} />
-        ))}
-      </div>
+      <SimpleBar className={s.listWrapper}>
+        <div className={s.list}>
+          {tasks?.map((task) => (
+            <TaskCard key={task.id} task={task} />
+          ))}
+        </div>
+      </SimpleBar>
     </div>
   );
 }
